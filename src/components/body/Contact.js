e5da2dded3e55f5ee4f98dd6ba6e2d66be0eb166
chadm9 <chadm_@yahoo.com>
2022-01-08 09:23:18 -0500

    submit confirm

--- src/components/body/Contact.js
+++ src/components/body/Contact.js
@@ -3,6 +3,7 @@ import BodyText from '../common/BodyText';
 import Title from '../common/Title';
 import { SEND_MAIL_CLOUD_FUNCTION_URL } from "../../config/config";
 import axios from 'axios';
+import Plug from "../common/Plug";
 
 const Contact = () => {
     return (
@@ -15,23 +16,24 @@ const Contact = () => {
 };
 
 const text = 'To schedule a trip, or for more information, contact Roger via email, telephone, or by filling out the form below (all fields are required).';
+const emailRegex = /\S+@\S+\.\S+/;
+const plugText = 'Email sent. Roger will be in contact with you soon!'
+
 
 const ContactForm = () => {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [message, setMessage] = useState('');
-    const [submitDisabled, setSubmitDisabled] = useState(false);
-    const [invalidName, setInvalidName] = useState(false);
-    const [invalidEmail, setInvalidEmail] = useState(false);
-    const [invalidMsg, setInvalidMsg] = useState(false);
-    const emailRegex = /\S+@\S+\.\S+/;
+    const [submitDisabled, setSubmitDisabled] = useState(true);
+    const [submitted, setSubmitted] = useState(null)
+
+    const validateForm = (value, handler) => {
+        handler(value);
+        if (name !== '' && message !== '' && emailRegex.test(email) && value !== '')  setSubmitDisabled(false);
+        else setSubmitDisabled(true);
+    }
 
     const handleSubmit = () => {
-        if (name.length === 0) setInvalidName(true);
-        if (!emailRegex.test(email)) setInvalidEmail(true);
-        if (message.length === 0) setInvalidMsg(true);
-        if (invalidName || invalidEmail || invalidMsg ) return setSubmitDisabled(true);
-
         const contents = JSON.stringify({
             name,
             email,
@@ -39,29 +41,32 @@ const ContactForm = () => {
         });
 
         axios.post(SEND_MAIL_CLOUD_FUNCTION_URL, contents, { headers:{'Content-Type': 'text/plain'} })
+        setSubmitted(true);
     }
 
-    return  (
-        <div className="col-sm-8 col-sm-offset-2">
+    const formMarkup =
             <form id="contact-form">
-                {/*<p id="form-submission"></p>*/}
                 <div className="form-group" id="name-input">
                     <label>Name</label>
-                    <input onChange={(e) => setName(e.target.value)} id='name' type="text" className="form-control" placeholder="name" value={name} />
-                    {invalidName ? <p id="form-error">A name is required</p> : null}
+                    <input onChange={(e) => validateForm(e.target.value, setName)} id='name' type="text" className="form-control" placeholder="name" value={name} />
                 </div>
                 <div className="form-group" id="email-input">
                     <label>Email Address</label>
-                    <input type="email" className="form-control" id="email" placeholder="email address" value={email} onChange={(e) => setEmail(e.target.value) } />
-                    {invalidEmail ? <p id="form-error">An email address is required</p> : null}
+                    <input type="email" className="form-control" id="email" placeholder="email address" value={email} onChange={(e) => validateForm(e.target.value, setEmail) } />
+                    {/*{invalidEmail ? <p id="form-error">A valid email address is required</p> : null}*/}
                 </div>
                 <div className="form-group">
                     <label>Message</label>
-                    <textarea id="message" placeholder="message" className="form-control" rows="4" value={message} onChange={(e) => setMessage(e.target.value)} />
-                    {invalidMsg ? <p id="form-error">A message is required</p> : null}
+                    <textarea id="message" placeholder="message" className="form-control" rows="4" value={message} onChange={(e) => validateForm(e.target.value, setMessage)} />
                 </div>
                 <button id='contact-button' className="btn btn-primary" disabled={submitDisabled} onClick={handleSubmit}>Submit</button>
             </form>
+
+    const submittedMarkup = <Plug text={plugText} />
+
+    return  (
+        <div className="col-sm-8 col-sm-offset-2">
+            {submitted ? submittedMarkup : formMarkup}
         </div>
     )
 }
